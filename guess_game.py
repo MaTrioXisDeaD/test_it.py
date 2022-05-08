07491ca626391576cd2674c6d5d4e5ebaca1f745
mian312 <102272538+mian312@users.noreply.github.com>
2022-05-08 21:10:55 +0530

    Create guess_game.py

---
+++ guess_game.py
@@ -0,0 +1,55 @@
+def to_infinity():
+    index = 0
+    while True:
+        yield index
+        index += 1
+
+def compare(z,a):
+    if z < a:
+        print("Please try a larger number")
+    elif z > a:
+        print("Please try a smaller number")
+
+
+print("Hey there! Would you like to play a new game ?")
+c = input("Type yes or no: ").lower()
+for i in to_infinity():
+    if c == "yes":
+        print('''Okey then user! 
+        This is a new game "Guess the number" 
+        The certain rules of this game are:
+         1. There is a secret number between 1-50, which you have to guess.
+         2. you will be given 7 chance to guess the number.
+         3. After each of your attempt you will be given the hint weather your inputted number is larger or smaller than the secret number.
+         4. If you print any other type of data-type other than 'integer' you will lost the game. ''')
+        a = 21
+        print("Let's start the game.")
+        try:
+            for i in range(7):
+                z = input("Enter a number: ")
+                z = int(z)
+                if z == a :
+                    print("Yah! you won the game.")
+                    break
+                elif z != a :
+                    compare(z,a)
+        except:
+            print("OOPs! Ypu lost the game.")
+        finally:
+            print("Hope to se you again.")
+        try:
+            if z != int(z):
+                print("Hey! you are suppose to type integer number.")
+        except:
+            break
+    elif c == "no":
+        print("Ok! See you next time.")
+        break
+    else:
+        print("Keyword not detected.")
+        c = input("Type yes or no: ").lower()
+    if i == 3:
+        continue
+
+
+
